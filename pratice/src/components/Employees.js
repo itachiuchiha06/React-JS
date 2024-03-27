0a0aae9979feb119bdba2c6e556daf01aa16d537
itachiuchiha <guntupalli.charan.77@gmail.com>
2024-03-27 18:41:23 +0530

    First Commit

---
+++ pratice/src/components/Employees.js
@@ -0,0 +1,25 @@
+const Employees =[
+    {
+        id:"1",
+        Name:"charan",
+        Age:"24"
+    },
+    {
+        id:"2",
+        Name:"Ram",
+        Age:"21"
+    },
+    {
+        id:"3",
+        Name:"dev",
+        Age:"23"
+    },
+    {
+        id:"4",
+        Name:"vasu",
+        Age:"26"
+    }
+
+]
+
+export default Employees;
